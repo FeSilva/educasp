7de0ead7f3d5bc061e78a1889d9c5f16939e57d9
José Guilherme Sório <joseguilhermesorio@gmail.com>
2021-11-16 22:22:15 -0300

    Ajustes CSS documento OS

--- resources/views/documents/os.blade.php
+++ resources/views/documents/os.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/os.css') }}">
     </head>
     <body>
         <div class="container">
