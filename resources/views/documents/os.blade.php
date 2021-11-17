4cc209799688b451bb154110bb30afac583bc0f6
Felipe Feitosa <felipe.feitosa@telium.com.br>
2021-11-17 18:06:06 -0300

    yarn

--- resources/views/documents/os.blade.php
+++ resources/views/documents/os.blade.php
@@ -5,13 +5,81 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="{{ asset('css/os.css') }}">
     </head>
+    <style>
+        .input-escondido, .input-escondido:focus, .input-escondido:active {
+            border: transparent;
+            outline: transparent;
+            cursor: not-allowed;
+            background: #FFF !important;
+        }
+        
+        select.form-control:not([size]):not([multiple]) {
+            height: 42px;
+        }
+        
+        .form-group input[type=file] {
+            opacity: 1;
+            height: 42px;
+        }
+        
+        table .table-borderd {
+            border: 1px solid #CCC;
+        }
+        .divisor {
+            height: 5px;
+            background-color: black;
+            width: 100%;
+            margin: 20px 0;
+        }
+        
+        table tr td {
+            text-transform: uppercase;
+        }
+        
+        table .thead {
+            font-weight: bold;
+        }
+        
+        .assinatura {
+            margin-top: 200px;
+            text-transform: uppercase;
+            font-weight: bold;
+            border-top: 1px solid #444;
+            max-width: 300px;
+        }
+        
+        .data, .de_acordo {
+            font-weight: bold;
+            margin-top: 40px;
+        }
+        
+        .data .divisor_data {
+            padding: 0 20px;
+        }
+        
+        .assinatura_supervisao, .assinatura_gerencia {
+            margin-top: 100px;
+            text-transform: uppercase;
+            font-weight: bold;
+            border-top: 1px solid #444;
+            max-width: 300px;
+        }
+        
+        .assinatura_gerencia_span {
+            position: relative;
+            top: 80px;
+        }
+        
+        .assinatura_gerencia {
+            margin-top: 80px;
+        }
+    </style>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <img src="http://www.fde.sp.gov.br/imagens/logo_FDE.png" alt="Logo" class="img-responsive">
+                    <img src="{{asset('paper').'/img/Logo_fde.jpg'}}" alt="LogoTeste" class="img-responsive">
                 </div>
                 <div class="divisor"></div>
             </div>
